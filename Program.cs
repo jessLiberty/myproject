71e3700b175eb5a42000664ec0ee2976373ef5b1
Alexey Skorokhod <skorhodaleksej@gmail.com>
2023-02-18 16:59:06 +0300

    первый коммит

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Emgu.CV;
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mat pic = new Mat();
+            pic = CvInvoke.Imread(@"C:\Users\User\source\repos\ConsoleApp56\night.jpg");
+            Mat gaussianBlur = new Mat();
+
+            CvInvoke.GaussianBlur(pic, gaussianBlur, new System.Drawing.Size(pic.Rows, pic.Cols), 1);
+
+            CvInvoke.Imshow("starry night", pic);
+            CvInvoke.Imshow("blurry night", gaussianBlur);
+
+            CvInvoke.WaitKey();
+        }
+    }
+}
